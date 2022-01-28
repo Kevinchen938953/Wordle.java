13582e13a576268d5bb03867202f8d6dbbd9df96
houjer23 <60145875+houjer23@users.noreply.github.com>
2022-01-28 09:35:59 -0500

    Create Wordle.java

---
+++ Wordle.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Wordle {
+	public static void main(String args[]) throws IOException{
+		Wordle_Input_File input_dictionary = new Wordle_Input_File();
+		
+	}
+}
